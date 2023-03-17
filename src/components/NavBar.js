817b6780984dcd0c7675175e7f37589f9c26e096
kingjessie <jessie.kinganga@gmail.com>
2023-03-17 14:33:27 +0000

    draft skeleton work

---
+++ src/components/NavBar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <header className="fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
+    <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+    <ul className="nav flex space-x-4">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="about"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          About
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="services"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Services
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+    </div>
+    </header>
+  );
+}
+
+export default NavBar;
