212185cc29f304e05b263a5e2dc6f1f6b66f492a
kingjessie <jessie.kinganga@gmail.com>
2023-03-20 14:45:43 +0000

    home page content added, footer added and styled, also connected to APP.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,17 +1,25 @@
 import React from "react";
-import "./home.css"
-
+import "./home.css";
 
 function Home() {
   return (
     <div className="home pt-24">
       <div className="container mx-auto h-full relative pt-24">
         <div className="flex flex-col justify-center">
-            <h1 className="h1-home lg:text-[80px] text-white">
-              <span className="h1-span">Wellness</span> <br />   Warrior
-            </h1>
-    </div>
-    </div>
+          <h1 className="h1-home lg:text-[85px] text-white">
+            <span className="h1-span">Wellness</span> <br /> Warrior
+          </h1>
+            <p class=" p-home  text-white  mb-4">
+              Welcome to our wellness website, where we strive to help you
+              achieve a balanced and fulfilling life.
+            </p>
+            <p class="p-home  text-white mb-4">
+              We're here to help you improve your physical fitness, manage
+              stress and anxiety, or find peace and tranquility in your everyday
+              life.
+            </p>
+        </div>
+      </div>
     </div>
   );
 }
