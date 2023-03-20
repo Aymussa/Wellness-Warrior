98b0389baf236e2516eb1ee3633501b1bdd8ab6f
kingjessie <jessie.kinganga@gmail.com>
2023-03-20 15:40:29 +0000

    button added on home, issue with styling at the mommeny

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,26 +1,28 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import "./home.css";
 
 function Home() {
   return (
     <div className="home pt-24">
       <div className="container mx-auto h-full relative pt-24">
-        <div className="flex flex-col justify-center">
+        
           <h1 className="h1-home lg:text-[85px] text-white">
             <span className="h1-span">Wellness</span> <br /> Warrior
           </h1>
-            <p class=" p-home  text-white  mb-4">
-              Welcome to our wellness website, where we strive to help you
-              achieve a balanced and fulfilling life.
-            </p>
-            <p class="p-home  text-white mb-4">
+            <p className="p-home  text-white mb-4">
               We're here to help you improve your physical fitness, manage
               stress and anxiety, or find peace and tranquility in your everyday
               life.
             </p>
+            <p className="p-home1  text-white mb-4"> Let's see how we can help you by taking our assessment:
+        </p>
+        <Link to="/services" className="btn-1">
+      Assessment
+    </Link>
         </div>
       </div>
-    </div>
+  
   );
 }
 
